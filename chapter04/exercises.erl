e073b46b6dea99589953c8a3bbb2aaa9195f8e74
Dustin Koupal <dustink@gmail.com>
2014-04-19 14:22:46 -0700

    finishing ex3

--- chapter04/exercises.erl
+++ chapter04/exercises.erl
@@ -5,8 +5,25 @@ my_tuple_to_list(T) when is_tuple(T) -> my_tuple_to_list(T, 1).
 my_tuple_to_list(T, Size) when Size > tuple_size(T) -> [];
 my_tuple_to_list(T, Size) -> [element(Size, T)|my_tuple_to_list(T,Size+1)].
 
+%% Returns seconds elapsed as a float
+my_time_func(F) ->
+    {Smeg, Ss, Smic} = now(),
+    F(),
+    {Emeg, Es, Emic} = now(),
+    ((Emeg - Smeg) * 1.0e6) +
+    (Es - Ss) +
+    ((Emic - Smic) / 1.0e6).
+
+my_date_string() ->
+    {Year, Month, Day}        = date(),
+    {Hours, Minutes, Seconds} = time(),
+    io:format("~p-~2..0B-~2..0B ~2..0B:~2..0B:~2..0B~n",
+               [Year, Month, Day, Hours, Minutes, Seconds]).
 test() ->
     Foo = {this, is, a, {test, {this, too}}},
     BIFList = erlang:tuple_to_list(Foo),
     BIFList = my_tuple_to_list(Foo),
+    Sleep = fun() -> timer:sleep(500) end,
+    io:format("~p~n",[my_time_func(Sleep)]),
+    my_date_string(),
     passed.
