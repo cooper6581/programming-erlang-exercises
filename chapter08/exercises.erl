61c439cb98a632ba2ff9a5aa95bb9ab395979840
Dustin Koupal <dustink@gmail.com>
2014-04-29 01:12:12 +0000

    adding more to ex2

--- chapter08/exercises.erl
+++ chapter08/exercises.erl
@@ -23,9 +23,39 @@ max_functions([{HModule, Length}|T], {_, Max}) when Length > Max ->
 max_functions([{_, Length}|T], {CModule, Max}) when Length < Max ->
     max_functions(T, {CModule, Max}).
 
+%% Cleaner, but requires a sort on the list
+max_functions_2(Modules) ->
+    lists:last(lists:keysort(2, Modules)).
+
+%% Answers the most common function question.
+%% Changed to return top 3 (#1 was module_info)
+most_common_function(Info) ->
+    % strip the arity, so we have a list of function names
+    Functions = [ Fname || {Fname, _} <- extract_exports(Info) ],
+    Counts = list_frequency(Functions),
+    Sorted = lists:keysort(2,Counts),
+    lists:reverse(lists:nthtail(length(Sorted) - 3, Sorted)).
+
+%% create a list of all functions exported by modules
+extract_exports(Info) ->
+    lists:flatten([ Exports || {_Module, [{exports, Exports}|_]} <- Info]).
+
+%% returns tagged tuple {item, count}
+list_frequency(L) -> list_frequency(L, #{}).
+list_frequency([], Acc) -> maps:to_list(Acc);
+list_frequency([Key|T], Acc) ->
+    case maps:find(Key, Acc) of
+        {ok, Value} ->
+            list_frequency(T, maps:update(Key, Value+1, Acc));
+        error ->
+            list_frequency(T, maps:put(Key, 1, Acc))
+    end.
+
 test() ->
     io:format("Functions in dict module: ~p~n",[functions_in_dict()]),
     Info = get_module_info(),
     Fun_count = get_max_functions(Info),
     io:format("Max Functions: ~p~n", [max_functions(Fun_count)]),
+    io:format("Max Functions (2nd version): ~p~n", [max_functions_2(Fun_count)]),
+    io:format("Most Common Function(s): ~p~n", [most_common_function(Info)]),
     passed.
