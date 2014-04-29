f52bdac132e5b98b511f52fb3f91c95b2b4a6d63
Dustin Koupal <dustink@gmail.com>
2014-04-28 20:37:35 -0700

    finishing last ex

--- chapter08/exercises.erl
+++ chapter08/exercises.erl
@@ -31,25 +31,37 @@ max_functions_2(Modules) ->
 %% Changed to return top 3 (#1 was module_info)
 most_common_function(Info) ->
     % strip the arity, so we have a list of function names
-    Functions = [ Fname || {Fname, _} <- extract_exports(Info) ],
-    Counts = list_frequency(Functions),
+    Counts = list_frequency_dict(functions_to_list(Info)),
     Sorted = lists:keysort(2,Counts),
     lists:reverse(lists:nthtail(length(Sorted) - 3, Sorted)).
+
+functions_to_list(Info) ->
+    [ Fname || {Fname, _} <- extract_exports(Info) ].
 
 %% create a list of all functions exported by modules
 extract_exports(Info) ->
     lists:flatten([ Exports || {_Module, [{exports, Exports}|_]} <- Info]).
 
 %% returns tagged tuple {item, count}
-list_frequency(L) -> list_frequency(L, #{}).
-list_frequency([], Acc) -> maps:to_list(Acc);
-list_frequency([Key|T], Acc) ->
-    case maps:find(Key, Acc) of
-        {ok, Value} ->
-            list_frequency(T, maps:update(Key, Value+1, Acc));
-        error ->
-            list_frequency(T, maps:put(Key, 1, Acc))
-    end.
+%list_frequency(L) -> list_frequency(L, #{}).
+%list_frequency([], Acc) -> maps:to_list(Acc);
+%list_frequency([Key|T], Acc) ->
+%    case maps:find(Key, Acc) of
+%        {ok, Value} ->
+%            list_frequency(T, maps:update(Key, Value+1, Acc));
+%        error ->
+%            list_frequency(T, maps:put(Key, 1, Acc))
+%    end.
+
+%% same as above, using dict instead of map for older versions
+list_frequency_dict(L) -> list_frequency_dict(L, dict:new()).
+list_frequency_dict([], Acc) -> dict:to_list(Acc);
+list_frequency_dict([Key|T], Acc) ->
+    list_frequency_dict(T, dict:update_counter(Key, 1, Acc)).
+
+%% This is probably cheating...
+unambiguous(Info) ->
+    sets:to_list(sets:from_list(functions_to_list(Info))).
 
 test() ->
     io:format("Functions in dict module: ~p~n",[functions_in_dict()]),
@@ -58,4 +70,5 @@ test() ->
     io:format("Max Functions: ~p~n", [max_functions(Fun_count)]),
     io:format("Max Functions (2nd version): ~p~n", [max_functions_2(Fun_count)]),
     io:format("Most Common Function(s): ~p~n", [most_common_function(Info)]),
+    io:format("Unambiguous Functions: ~p~n", [unambiguous(Info)]),
     passed.
